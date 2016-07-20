4890fab727a656b75d386c2e649161c09df1b59a
Erwin Tandoc <erwin@bitscustomsoftware.com>
2016-07-20 18:53:11 +0800

    Updated to MVC
    Updated to MVC

--- webfiles/ezts/EZTS_Home.php
+++ webfiles/ezts/EZTS_Home.php
@@ -1,39 +1,29 @@
 <?php
+require '../../library.php';
+use Controllers\ViewController;
+use Controllers\LoginController;
+use Controllers\SidebarController;
+use Views\showPage;
 
-	require ('../../library.php');
-	
-	use Controller\LoginController;
-	use Controller\SidebarController;
-	use View\showPage;
-	use View\MainView;
-	use Model\AccessManagement\UserAccess;
-	use Model\UserManagement\UserInfo;
+$view = new ViewController;
+$auth = new LoginController;
+$sidebar = new SidebarController;
+$auth->auth();
+$show = new showPage;
 
-	$auth = new LoginController;
-	$sidebar = new SidebarController;
-	$view = new showPage;
-	$account = new UserAccess;
-	$mainView = new MainView;
-	$userinfo = new UserInfo;
 
-	$auth->auth($account);
-	//var_dump($_SESSION['userid']);
-	$view->showScripts();
-	$view->showHeader('EZTS Home');	
-	
-	$sidebar->showEztsSidebar();
-	$name = $userinfo->getUserName();
-	
 ?>
 
-	<div id="page-wrapper">
-		
-		<?php
-			$view->greet($name);
-			$mainView->DashboardBoxes();
-			
-		?>
-	</div>
+    <?php
+    echo $show->showHeader('TEST');
+    echo $show->showScripts();
 
-<?php $view->showFooter();?>
-</html>
+    ?>
+<body>
+    <div class="page-wrapper">
+        <div class = "container-fluid">
+        <?php echo $sidebar->showEztsSidebar(); ?>
+        test
+        </div>
+    </div>
+</body>
